69ddeb389342b8e23d57f3623b397b6f9bd51186
rahul-acr <rahul.saha.c@gmail.com>
2023-06-05 09:30:45 +0530

    Add story for strategy pattern

--- src/main/java/strategy/BasicExample.java
+++ src/main/java/strategy/BasicExample.java
@@ -4,10 +4,22 @@ public class BasicExample {
     class PaymentProcessor{
         void process(PaymentMethod paymentMethod, double amount){
             switch (paymentMethod){
-                case CREDIT_CARD -> System.out.println("Paying via credit card");
-                case DEBIT_CARD -> System.out.println("Paying via debit card");
-                case UPI -> System.out.println("Paying via UPI");
+                case CREDIT_CARD -> processCreditCardPayment(amount);
+                case DEBIT_CARD -> processDebitCardPayment(amount);
+                case UPI -> processUpiPayment(amount);
             }
         }
+    }
+
+    private static void processUpiPayment(double amount) {
+        System.out.println("Paying via UPI "+ amount);
+    }
+
+    private static void processDebitCardPayment(double amount) {
+        System.out.println("Paying via debit card "+ amount);
+    }
+
+    private static void processCreditCardPayment(double amount) {
+        System.out.println("Paying via credit card "+ amount);
     }
 }
